c72af2bf76af0239cd2a5fbcf9581fbda4407429
J Lucas <joaolucaspereira2002@gmail.com>
2024-12-29 19:16:33 -0300

    Criando rotas DELETE

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -15,7 +15,6 @@ def _create_user():
 def _list_users():
     query = db.select(User)
     users  = db.session.execute(query).scalars()
-    
     return [
         {
             'id': user.id ,
@@ -23,6 +22,8 @@ def _list_users():
         } 
         for user in users
     ]
+# (GET)   http://127.0.0.1:5000/users/
+# (POST)  http://127.0.0.1:5000/users/1
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_user():
@@ -31,28 +32,39 @@ def handle_user():
         return {'Post message': []}, HTTPStatus.CREATED
     else:
         return {'users': _list_users()}
+
+# (GET) http://127.0.0.1:5000/users/1
+@app.route('/<int:user_id>') 
+def get_user(user_id):
+    user = db.get_or_404(User, user_id)
+    return {
+        'id': user.id ,
+        'username': user.username
+    }
     
-
+    
+# (PATCH) http://127.0.0.1:5000/users/1
 @app.route('/<int:user_id>', methods=['PATCH']) 
-
 def update_user(user_id):
     user = db.get_or_404(User, user_id)
     data = request.json
-   
-    
+       
     mapper = inspect(User)
     for column in mapper.attrs:
         if column.key in data:
             setattr(user, column.key, data[column.key])
     db.session.commit()
-
-
-
-    return {
+    return{
         'id': user.id ,
         'username': user.username
     }
 
-
+# (DELETE) http://127.0.0.1:5000/users/1
+@app.route('/<int:user_id>', methods=['DELETE']) 
+def delete_user(user_id):
+    user = db.get_or_404(User, user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return "", HTTPStatus.NO_CONTENT
 
     
